e22d3849164ba4a9a5687dbca9a34677bf761213
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2024-11-26 20:13:48 -0500

    Muda comando do docker para back-end (finalizado)

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,22 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/routes';
-import { main, prisma } from './config/prisma';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
-// Cria os dados dos motoristas no banco diretamente, apenas para testar mais facilmente
-main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
 
 app.listen(8080, () => { console.log('Server running on port 8080') });
